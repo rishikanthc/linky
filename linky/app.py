d9344da8c15fd05a13164756b1494d85a8785208
rishikanthc <r3chandr@ucsd.edu>
2024-08-16 20:22:16 -0400

    URL shortener with time expiry

--- linky/app.py
+++ linky/app.py
@@ -11,11 +11,10 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse, JSONResponse
+from datetime import datetime, timedelta
 
 logger.add(sys.stdout, format="{time} {level} {message}", level="DEBUG")
-
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
@@ -26,17 +25,15 @@ app.add_middleware(
 package_dir = os.path.dirname(__file__)
 static_dir = os.path.join(package_dir, "static")
 fonts_dir = os.path.join(static_dir, "fonts/IBM_Plex")
-
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 app.mount("/fonts", StaticFiles(directory=fonts_dir), name="fonts")
 
 templates_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = Environment(loader=FileSystemLoader(templates_dir))
-
 templates = Jinja2Templates(directory="templates")
 templates.env = jinja_env
 
-# In-memory store for shortened URLs
+# In-memory store for shortened URLs with expiry times
 url_store = {}
 
 
@@ -51,20 +48,33 @@ async def render_page(request: Request):
 
 
 @app.post("/shorten")
-async def shorten_url(url: str = Form(...)):
+async def shorten_url(url: str = Form(...), expiry: str = Form(...)):
     short_url = generate_short_url()
     while short_url in url_store:
         short_url = generate_short_url()
-    url_store[short_url] = url
+
+    # Calculate expiry time
+    if expiry == "never":
+        expiry_time = None
+    else:
+        duration = int(expiry)  # Extract the number of seconds from the expiry string
+        expiry_time = datetime.now() + timedelta(seconds=duration)
+
+    url_store[short_url] = {"url": url, "expiry": expiry_time}
     short_url_full = f"http://localhost:8000/{short_url}"
     return JSONResponse(content={"short_url": short_url_full})
 
 
 @app.get("/{short_url}")
 async def redirect_to_original(short_url: str):
-    original_url = url_store.get(short_url)
-    if original_url:
-        return RedirectResponse(original_url)
+    url_data = url_store.get(short_url)
+    if url_data:
+        if url_data["expiry"] is None or datetime.now() < url_data["expiry"]:
+            return RedirectResponse(url_data["url"])
+        else:
+            # Remove expired URL
+            del url_store[short_url]
+            raise HTTPException(status_code=404, detail="Short URL has expired")
     else:
         raise HTTPException(status_code=404, detail="Short URL not found")
 
